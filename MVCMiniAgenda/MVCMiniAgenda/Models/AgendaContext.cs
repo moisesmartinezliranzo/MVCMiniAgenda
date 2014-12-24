2b700db7fd861b8cf4316981b77f0c4c80ca7f21
moisesmartinezliranzo <moisesmartinezliranzo@gmail.com>
2014-12-24 10:44:47 -0400

    Creacion de la clase AgendaContext y sus propiedades

---
+++ MVCMiniAgenda/MVCMiniAgenda/Models/AgendaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCMiniAgenda.Models
+{
+    public class AgendaContext
+    {
+        public DbSet<Usuarios> LosUsuarios { get; set; }
+    }
+}
