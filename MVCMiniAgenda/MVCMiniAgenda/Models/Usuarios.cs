624681934a11c0aa902d4980c1dcc2ce36194ad2
moisesmartinezliranzo <moisesmartinezliranzo@gmail.com>
2014-12-24 10:42:41 -0400

    Creacion del modelo Usuario y sus propiedades

---
+++ MVCMiniAgenda/MVCMiniAgenda/Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMiniAgenda.Models
+{
+    [Table("tblagenda")]
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
+    }
+}
